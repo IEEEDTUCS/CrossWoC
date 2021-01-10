274d5131d232c0ca412fc04ce22945ebace06824
sonalbera <sonalbera2003@gmail.com>
2021-01-10 14:37:21 +0530

    website overhaul

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 //counter //fuck jquery
-finaldate = new Date('14 March 2021');
+finaldate = new Date('13 February 2021');
 setInterval(() => {
     let timeleft = finaldate - new Date();
     d = document.getElementById('days');
